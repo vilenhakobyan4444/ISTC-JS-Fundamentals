64dd100abe28a249426388c03a694f317ebe16c1
Vladimir, Vilen Hakobyan <52594864+vilenhakobyan4444@users.noreply.github.com>
2019-07-14 10:58:57 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,1 +1,27 @@
-console.log("A");
+                                                     //forExample
+
+function myFunction() {
+  var text = "";
+  for (var i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+  }
+  document.write(text);
+}
+myFunction();
+
+
+                                                    //whileExample
+
+
+function myFunction() {
+  var text = "";
+  var i = 0;
+  while (i < 5) {
+    text += "<br>The number is " + i;
+    i++;
+  }
+  document.write(text);
+}
+myFunction();
+
+
